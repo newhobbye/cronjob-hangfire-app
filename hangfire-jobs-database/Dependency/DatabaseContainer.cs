2eb6bc81c63ebe8e55ea049101fea63ba4021324
Robson <robsonhobbye@gmail.com>
2024-07-24 13:54:32 -0300

    Adicionar arquivos de projeto.

---
+++ hangfire-jobs-database/Dependency/DatabaseContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace hangfire_jobs_database.Dependency
+{
+    public static class DatabaseContainer
+    {
+        public static void RegisterDatabaseContainer(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite("Data Source=localdatabase.db"));
+        }
+    }
+}
