da628096db7570f9cf89941d1ed554e350c22c9c
Robson <robsonhobbye@gmail.com>
2024-08-05 09:06:56 -0300

    Estrutura inicial dos testes

---
+++ hangfire-jobs-tests/Dependency/GetServiceInjection.cs
@@ -0,0 +1,28 @@
+﻿using hangfire_jobs_service.Dependency;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace hangfire_jobs_tests.Dependency
+{
+    public static class GetServiceInjection
+    {
+        public static T GetService<T>()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile(".\\appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+            serviceCollection.AddSingleton(configuration);
+
+            ServiceContainer.RegisterServiceContainer(serviceCollection); //, configuration
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            T? getService = serviceProvider.GetRequiredService<T>();
+
+            return getService;
+        }
+    }
+}
---
+++ hangfire-jobs-tests/Dependency/Services/RequestServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using hangfire_jobs_service.Interfaces;
+
+namespace hangfire_jobs_tests.Dependency.Services
+{
+    public class RequestServiceTests
+    {
+        private readonly IRequestsService _requestsService;
+
+        public RequestServiceTests()
+        {
+            _requestsService = GetServiceInjection.GetService<IRequestsService>();
+        }
+
+        [Fact(DisplayName = "Get na api do viaCep")]//, Skip = "Desativar xTests azure"
+        public async Task GetViaCepDataAsync()
+        {
+            string cep = "06448150";
+
+            var result = await _requestsService.GetViaCepDataAsync(cep);
+
+            Assert.NotNull(result);
+        }
+    }
+}
