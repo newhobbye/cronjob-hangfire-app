42964496205d3b85ec52352bca9448fb03689781
Robson <robsonhobbye@gmail.com>
2024-08-07 09:19:59 -0300

    Config inicial do hangfire

--- hangfire-jobs-service/Dependency/ServiceContainer.cs
+++ hangfire-jobs-service/Dependency/ServiceContainer.cs
@@ -1,7 +1,13 @@
-﻿using hangfire_jobs_database.Dependency;
+﻿using Hangfire.Dashboard;
+using Hangfire;
+using hangfire_jobs_database.Dependency;
 using hangfire_jobs_service.Interfaces;
 using hangfire_jobs_service.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using hangfire_jobs_service.Filters;
 
 namespace hangfire_jobs_service.Dependency
 {
@@ -13,6 +19,33 @@ namespace hangfire_jobs_service.Dependency
 
             services.AddScoped<IRequestsService, RequestsService>();
             services.AddScoped<IUserService, UserService>();
+        }
+
+        public static void RegisterServiceAppContainer(WebApplication app, IConfiguration configuration)
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] { new MyAuthorizationFilter() },
+                IsReadOnlyFunc = (DashboardContext context) => true
+            });
+        }
+
+        private static void HangfireJobsConfiguration(IServiceCollection services, IConfiguration configuration)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+
+            var userService = serviceProvider.GetRequiredService<IUserService>();
+
+            RecurringJob.AddOrUpdate("Criar kyc para estabelecimento homologado",
+                () => userService.VerifyAddressesOfUsersAsync(),
+                configuration.GetValue<string>("HangfireOperationSettings:CronHourByHour"));
+
+            RecurringJob.AddOrUpdate("Criar kyc para estabelecimento onboarding sem kyc",
+                () => userService.ResetUsersForHangfireOperationAsync(),
+                configuration.GetValue<string>("HangfireOperationSettings:CronHourByHour"));
+
+
+            serviceProvider.Dispose();
         }
     }
 }
---
+++ hangfire-jobs-service/Filters/MyAuthorizationFilter.cs
@@ -0,0 +1,15 @@
+﻿using Hangfire.Dashboard;
+
+namespace hangfire_jobs_service.Filters
+{
+    public class MyAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            //var httpContext = context.GetHttpContext();
+
+            //return httpContext.User.Identity?.IsAuthenticated ?? false;
+            return true;
+        }
+    }
+}
